07589534918e8d3b4760f7df3300545288bc5bca
asim <asim.19824@gmail.com>
2022-11-28 18:50:53 +0500

    sddsf

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //  return view('welcome'); 
+     return view('auth.login');
+    
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
