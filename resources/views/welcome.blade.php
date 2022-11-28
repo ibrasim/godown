3496bfc4e16f5f7967ffd627a2003b54d6a4f539
asim <asim.19824@gmail.com>
2022-11-28 07:27:58 +0500

    sdfsdf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -102,13 +102,28 @@
                         <div class="flex items-center">
                             
 
+
+
+                            @if (Route::has('login'))
+                            
+                                @auth
+                                    <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                                @else
+                                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+            
+                                    @if (Route::has('register'))
+                                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                                    @endif
+                                @endauth
+                            
+                        @endif
+
+
                             
 
                           
 
-                            <a href="{{ route('login') }}" class="ml-1 underline">
-                            Login
-                            </a>
+                          
                             
                         </div>
                     </div>
