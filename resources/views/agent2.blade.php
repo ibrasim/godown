f9b7295e690a8c2dfe1e6cc74e7d3eb011031a00
asim <asim.19824@gmail.com>
2022-11-30 21:40:25 +0500

    sdf

--- resources/views/agent2.blade.php
+++ resources/views/agent2.blade.php
@@ -5,12 +5,29 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('RTAG03') }}</div>
 
                 <div class="card-body">
                     
                
-                 <h2>   {{ 'Agent2s page' }}</h2>
+               
+                 <img src="{{ asset('/lpant.jpg') }}" />
+                 <h2>   {{ __('MVR 5.00') }}</h2>
+                 <br>
+                 <img src="{{ asset('/jersey.jpg') }}" />
+                 <h2>   {{ __('MVR 5.00') }}</h2>
+                 <br>
+                 <img src="{{ asset('/lshoes.jpg') }}" />
+                 <h2>   {{ __('MVR 5.00') }}</h2>
+                 <br>
+                 <img src="{{ asset('/lbag.jpg') }}" />
+                 <h2>   {{ __('MVR 5.00') }}</h2>
+                 <br>
+                 <img src="{{ asset('/bedsheet.jpg') }}" />
+                 <h2>   {{ __('MVR 5.00') }}</h2>
+      
+      
+      
                 </div>
             </div>
         </div>
