df2b13f991a7f4af3d0592bef0711d64c867116a
Ibrahim Asim <asim.19824@gmail.com>
2022-12-07 08:42:12 +0700

    sdfsdf

--- resources/views/addcustomer.blade.php
+++ resources/views/addcustomer.blade.php
@@ -3,31 +3,142 @@
 @section('content')
 
 <div class="container">
-  <h2>Customer Table</h2>
+  <h2>purchased itms table</h2>
   <table class="table">
     <thead>
       <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
+        <th>Item</th>
+        <th>Description</th>
+        <th>Quantity</th>
+        <th>Unit price</th>
+        <th>Total</th>
+        <th>Purchased Date</th>
+        
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
+      <td>Tshirt</td>
+        <td>long sleeve</td>
+ 	    <td>12</td>
+        <td>150</td>
+ 	    <td>1800.00</td>
+         <td>4 dec</td>
       </tr>
       <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
+         <td>Tshirt</td>
+         <td>short sleeve</td>
+ 	    <td>12</td>
+        <td>150</td>
+ 	    <td>1800.00</td>
+         <td>4 dec</td>
       </tr>
       <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
+        <td>Ladies pants</td>
+        <td>black stretchabe different sizes</td>
+ 	    <td>24</td>
+        <td>75</td>
+ 	    <td>1800.00</td>
+         <td>4 dec</td>
       </tr>
+      <tr>
+        <td>Ladies hand bags</td>
+        <td>various design and sizes</td>
+ 	    <td>22</td>
+        <td>100</td>
+ 	    <td>2200.00</td>
+         <td>4 dec</td>
+      </tr>
+      <tr>
+        <td>Ladies hand bags</td>
+        <td>various design and sizes</td>
+ 	    <td>30</td>
+        <td>110</td>
+ 	    <td>3300.00</td>
+         <td>4 dec</td>
+         
+      </tr>
+      <tr>
+      <td>Ladies hand bags</td>
+        <td>various design and sizes</td>
+ 	    <td>15</td>
+        <td>55</td>
+ 	    <td>825.00</td>
+         <td>4 dec</td>
+         </tr>
+      <tr>
+         <td>Ladies hand bags</td>
+        <td>various design and sizes</td>
+ 	    <td>36</td>
+        <td>30</td>
+ 	    <td>1080.00</td>
+         <td>4 dec</td>
+         </tr>
+         <tr>
+         <td>Baby Towel </td>
+        <td>small</td>
+ 	    <td>4</td>
+        <td>100</td>
+ 	    <td>400.00</td>
+         <td>4 dec</td>
+         </tr>
+         <tr>
+         <td>Baby Towwel </td>
+        <td>smlarge</td>
+ 	    <td>2</td>
+        <td>150</td>
+ 	    <td>300.00</td>
+         <td>4 dec</td>
+         </tr>
+         <tr>
+         <td>Lunch Box </td>
+        <td>pink</td>
+ 	    <td>6</td>
+        <td>50</td>
+ 	    <td>300.00</td>
+         <td>4 dec</td>
+         </tr>
+         <tr>
+         <td>Ladies pants </td>
+        <td>differnt colours stretchable</td>
+ 	    <td>12</td>
+        <td>120</td>
+ 	    <td>1440.00</td>
+         <td>6 dec</td>
+         </tr>
+         <tr>
+         <td>Track suit</td>
+        <td>men normal quality</td>
+ 	    <td>6</td>
+        <td>80</td>
+ 	    <td>480.00</td>
+         <td>6 dec</td>
+         </tr>
+         <tr>
+         <td>Track suit</td>
+        <td>men good quality</td>
+ 	    <td>6</td>
+        <td>120</td>
+ 	    <td>720.00</td>
+         <td>6 dec</td>
+         </tr>
+
+         <tr>
+         <td>Socks</td>
+        <td>men </td>
+ 	    <td>6</td>
+        <td>150</td>
+ 	    <td>900.00</td>
+         <td>6 dec</td>
+         </tr>
+         <tr>
+         <td>Socks</td>
+        <td>men </td>
+ 	    <td>3</td>
+        <td>120</td>
+ 	    <td>360.00</td>
+         <td>6 dec</td>
+         </tr>
     </tbody>
   </table>
 </div>
