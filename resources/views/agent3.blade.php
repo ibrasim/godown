c0d825df715bfddad5be0a5f5256e6e060b2eaf8
asim <asim.19824@gmail.com>
2023-01-05 21:28:55 +0500

    sdfsdf

--- resources/views/agent3.blade.php
+++ resources/views/agent3.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Sem -2 short notes') }}</div>
+                <div class="card-header">{{ __('Short notes for MCQ and EXAM. This is just draft copied form slide nned to be edited. All thanks to our lecturers who provided an oean of wonderful study materials (slides,books, links notes) for us!') }}</div>
                 {{-- <div style = "padding:35px;">  --}}
                 
 
@@ -16,29 +16,21 @@
                       
                       
                         <h2>CRIMINAL LAW</h2>
-                      <h4>week-1 Assult and Battery </h4>
+                      <h4>Week-1 Assult and Battery </h4>
+                      <p class="p-4 bg-primary text-white">
+                        “I take no responsibility or liability for any errors or omissions in the content of this page. The information is collected / copied from lecture notes and other sources. There are no guarantees of completeness, accuracy" S1409011639 </p>
+                        
                       <p class="p-4 bg-success text-white">
                 
 
-Assault' means physical attack against the body of another person resulting in serious bodily injury; excluding indecent/sexual assault; threats and slapping/punching. 'Assault' leading to death should also be excluded.<br>
-(1) touches or injures such person, or  (2) puts such person in fear of imminent bodily injury.<br>
-Ingredients of assult - intended to cuase apprehensionof harmful or offensive contact and the act indeed caused
-appreshesion in the victim that harmful or offensive contact wourld occur <br>
-Actus of assult: the victim must apprehend immeidate, unlaowrul, personal violence<br>
-What Are the Differences Between Simple and Aggravated Assault? Simple assault is the least serious form of assault and usually involves minor injury or a limited threat of violence. Aggravated assault involves circumstances that make the crime more serious, as when the victim is threatened with a weapon or experiences violence amounting to significantly more than a minor slap across the face or a punch in the jaw <br>
-comparision of assult / battery :There are two different types of assault: common assault and battery. Common assault is when apprehension of immediate unlawful violence is caused.
- Battery is when unlawful violence on another person is exerted. Successful claims for both common assault and battery must prove that the actions were committed with intention or recklessness.  <br>
- battery:Battery is, in many ways, the completion of an assault. Battery is defined as an intentional offensive or harmful touching of another person that is done without his or her consent. Since an assault is the threatening of harm, and a battery is the actual act of harm, the two crimes are often charged together.<br>
+                        Assault means physical attack against the body of another person resulting in serious bodily injury; excluding indecent/sexual assault. 'Assault' leading to death should also be excluded. (1) touches or injures such person, or (2) puts such person in fear of imminent bodily injury. Ingredients of assault - intended to cause apprehension of harmful or offensive contact and the act indeed caused apprehension in the victim that harmful or offensive contact would occur Actus of assault: the victim must apprehend immediate, unlawful, personal violence <mark>What Are the Differences Between Simple and Aggravated Assault? Simple assault is the least serious form of assault and usually involves minor injury or a limited threat of violence. Aggravated assault involves circumstances that make the crime more serious, as when the victim is threatened </mark>with a weapon or experiences violence. <mark>Comparison of assault / battery: There are two different types of assault: common assault and battery. </mark>Common assault is when apprehension of immediate unlawful violence is caused. Battery is when unlawful violence on another person is exerted. Successful claims for both common assault and battery must prove that the actions were committed with intention or recklessness. Battery is, in many ways, the completion of an assault. Battery is defined as an intentional offensive or harmful touching of another person that is done without his or her consent. Since an assault is the threatening of harm, and a battery is the actual act of harm, the two crimes are often charged together.
 </p>
 
- <h4>week-2 sexual offences </h4>
+ <h4>Week-2 Sexual Offences </h4>
  <p class="p-4 bg-danger text-white">
-Meaning: A sexual offence occurs when your physical body is interfered with in an inappropriate way by another person, that is when a person deliberately touches any part of your body including [by that person's touching] your private parts in a sexual manner or way and/or by sexual intercourse, all without your permission or consent.<br>
-sexual offence: “Sexual Assault” means the touching of a sexual organ of a person, with or without a sexual intent by another person, with or without the use of an object or a tool, without consent, and in a manner, which would not constitute it a Rape or Sexual Injury, where the two persons are not married. Or it is forcing a person to reveal his sexual organ without his consent. Or causing an injury to a person’s sexual organ without his consent. Or causing any other degrading act on the sexual organ of a person’s sexual organ without his consent. <br>
-types of sexual offences :1) Rape 2) Sexual Assault by penetration 3) Sexual Assault <br>
-“Rape” means the insertion, however minute, of a person’s sexual organ into a sexual organ or an organ which is not a sexual organ of another person without consent, where the two persons are not married <br>
-exceptions of sexual offence act :A husband having sexual intercourse with his wife without her consent shall be considered marital rape under the following exceptional circumstances <br>
-s, the completion of an assault. Battery is defined as an intentional offensive or harmful touching of another person that is done without his or her consent. Since an assault is the threatening of harm, and a battery is the actual act of harm, the two crimes are often charged together.<br>
+    A sexual offence occurs when your physical body is interfered with in an inappropriate way by another person, that is when a person <mark>deliberately touches any part of your body including [by that person's touching your private parts in a sexual manner </mark> or way and or by sexual intercourse, all without your permission or consent. sexual offence: “Sexual Assault” means the touching of a sexual organ of a person, with or without a sexual intent by another person, with or without the use of an object or a tool, without consent, and in a manner, which would not constitute it a Rape or Sexual Injury, where the two persons are not married. Or it is forcing a person to reveal his sexual organ without his consent. Or causing an injury to a person’s sexual organ without his consent. Or causing any other degrading act on the sexual organ of a person’s sexual organ without his consent. <mark> Types of sexual offences :1) Rape 2) Sexual Assault by penetration 3) Sexual Assault</mark>
+    Rape means the insertion, however minute, of a person’s sexual organ into a sexual organ or an organ which is not a sexual organ of another person without consent, where the two persons are not married exceptions of sexual offence act : <mark> A husband having sexual intercourse with his wife without her consent shall be considered marital rape </mark>  under the following exceptional circumstances, the completion of an assault. Battery is defined as an intentional offensive or harmful touching of another person that is done without his or her consent. <mark>Since an assault is the threatening of harm, and a battery is the actual act of harm, </mark> the two crimes are often charged together.
+    
 </p>
 <h4>week-3 Homicide </h4>
 <p class="p-4 bg-success text-white">
