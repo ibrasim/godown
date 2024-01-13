455a7eba56796a9120e3a15492e59f78d88c7f77
ibrasim <36990878+ibrasim@users.noreply.github.com>
2024-01-13 14:05:01 +0500

    sdfsdf

--- resources/views/agent3.blade.php
+++ resources/views/agent3.blade.php
@@ -89,6 +89,103 @@
                             <td>rent with utility bills (as agreed)</td>
                           </tr>
                           
+                          <tr>
+                            <th scope="row">2024</th>
+                            <td>fairview A</td>
+                            <td>August </td>
+                            <td>monthly</td>
+                            <td>4-Aug-23</td>
+                            <td>Dr.Omar</td>
+                            <td>7000.00</td>
+                            <td>cash hand over</td>
+                            <td>rent</td>
+                          </tr>
+
+                          <tr>
+                            <th scope="row">2024</th>
+                            <td>fairview A</td>
+                            <td>July </td>
+                            <td>monthly</td>
+                            <td>9-Jul-23</td>
+                            <td>Dr.Omar</td>
+                            <td>7000.00</td>
+                            <td>cash hand over</td>
+                            <td>rent</td>
+                          </tr>
+
+                          <tr>
+                            <th scope="row">2024</th>
+                            <td>fairview A</td>
+                            <td>June </td>
+                            <td>monthly</td>
+                            <td>4-Jun-23</td>
+                            <td>Dr.Omar</td>
+                            <td>10500.00</td>
+                            <td>cash hand over</td>
+                            <td>rent with security deposit</td>
+                          </tr>
+
+                          <tr>
+                            <th scope="row">2024</th>
+                            <td>fairview A</td>
+                            <td>May </td>
+                            <td>monthly</td>
+                            <td>26-May-23</td>
+                            <td>Dr.Omar</td>
+                            <td>3500.00</td>
+                            <td>cash hand over</td>
+                            <td>advance partly</td>
+                          </tr>
+                            
+                          <tr>
+                            <th scope="row">2024</th>
+                            <td>fairview A</td>
+                            <td>April </td>
+                            <td>monthly</td>
+                            <td>3-Apr-23</td>
+                            <td>Dr.Savana</td>
+                            <td>6000.00</td>
+                            <td>transfer</td>
+                            <td>rent</td>
+                          </tr>
+
+                          <tr>
+                            <th scope="row">2024</th>
+                            <td>fairview A</td>
+                            <td>March </td>
+                            <td>monthly</td>
+                            <td>1-Mar-23</td>
+                            <td>Dr.Savana</td>
+                            <td>6000.00</td>
+                            <td>transfer</td>
+                            <td>rent</td>
+                          </tr>
+
+                          <tr>
+                            <th scope="row">2024</th>
+                            <td>fairview A</td>
+                            <td>February</td>
+                            <td>monthly</td>
+                            <td>1-Feb-23</td>
+                            <td>Dr.Savana</td>
+                            <td>9000.00</td>
+                            <td>transfer</td>
+                            <td>rent with security deposit</td>
+                          </tr>
+
+                          <tr>
+                            <th scope="row">2024</th>
+                            <td>fairview A</td>
+                            <td>January</td>
+                            <td>half-monthly</td>
+                            <td>15-Jan-23</td>
+                            <td>Dr.Savana</td>
+                            <td>3000.00</td>
+                            <td>transfer</td>
+                            <td>rent</td>
+                          </tr>
+
+
                           </tr>
                         </tbody>
                       </table>
