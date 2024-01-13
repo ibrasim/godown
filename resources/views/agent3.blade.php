ddb4d3fd9e06c3ffa5c5237fec42d6355bb9e8f4
ibrasim <36990878+ibrasim@users.noreply.github.com>
2024-01-13 13:14:12 +0500

    sdfsdf

--- resources/views/agent3.blade.php
+++ resources/views/agent3.blade.php
@@ -32,13 +32,14 @@
                           <tr>
                             <th scope="row">2024</th>
                             <td>fairview A</td>
+                            <td>january</td>
                             <td>monthly</td>
                             <td>10-Jan-24</td>
                             <td>Dr.Omar</td>
                             <td>7812.59</td>
                             <td>transfer</td>
                             <td>rent with utility bills (as agreed)</td>
-                            <td>xx</td>
+                            
                           </tr>
                           <tr>
                             <th scope="row">2024</th>
