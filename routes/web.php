3ae61301d2a291e26d86c675b77902cdb7c17a6a
ibrasim <36990878+ibrasim@users.noreply.github.com>
2024-05-04 06:55:33 +0500

    30242024

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\customer;
-use App\Models\users;
+use App\Models\User;
 use App\Models\vendor;
 
 /*
@@ -16,7 +16,7 @@ use App\Models\vendor;
 |
 */
 Route::get('/all-tweets-csv', function () {
-    $table = users::all();
+    $table = user::all();
     $filename = "users.csv";
     $handle = fopen($filename, 'w+');
     fputcsv($handle, array('name', 'created at'));
