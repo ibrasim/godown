52e36c3125b61775a849edff54a813baa1700dfb
ibrasim <36990878+ibrasim@users.noreply.github.com>
2024-05-09 17:02:47 +0500

    30242024

--- resources/views/agent1.blade.php
+++ resources/views/agent1.blade.php
@@ -5,18 +5,18 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __(' 13 January 2024') }}</div>
+               
 
                 <div class="card-body">
                     
-               <div class="bg-primary"><h2>   {{ 'Tasks Completed and ready for testing' }}</h2></div>
+               
                  
                 </div>
                 <div>
                     
                 </div>
                 <div class="bg-warning">
-                  <h2>   {{ 'Tasks Todo - (in priority order)' }}</h2>
+                
                 </div>
                 
                 <div>
