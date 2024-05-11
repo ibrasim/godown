1faaa3f3c5389b0ef502b8829a803ac1a8e8aff1
ibrasim <36990878+ibrasim@users.noreply.github.com>
2024-05-11 17:56:29 +0500

    30242024

---
+++ database/migrations/2024_05_11_120947_create_tariffs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTariffsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tariffs', function (Blueprint $table) {
+            $table->id();
+            $table->string('chapter'); 
+            $table->string('sixdigit'); 
+            $table->string('hscode'); 
+            $table->string('desc'); 
+            $table->string('unit'); 
+            $table->string('mfnrate'); 
+            $table->string('saftrate'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tariffs');
+    }
+}
