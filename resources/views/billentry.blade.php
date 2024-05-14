4ca47ca07f96d24b210d6fadab31cadc7d307335
ibrasim <36990878+ibrasim@users.noreply.github.com>
2024-05-14 07:41:56 +0500

    30242024

--- resources/views/billentry.blade.php
+++ resources/views/billentry.blade.php
@@ -10,93 +10,49 @@
                             <div class="form-group row">
                                 
                                 <div class="col">
-                                     <input type="text"  name="vendorname" class="form-control" placeholder="Shop / Company / Individual Name">
+                                     <input type="text"  name="vendorname" class="form-control" placeholder="Qty">
                                 </div>
                                 <div class="col">
-                                     <input type="text" name="vendorregno" class="form-control" placeholder="Business Registration # If Individual ID #">
+                                     <input type="text" name="vendorregno" class="form-control" placeholder="Packing">
                                 </div>
                                 <div class="col">
-                                    <input type="text" name="vendoraddress"class="form-control" placeholder="Address  ">
+                                    <input type="text" name="vendoraddress"class="form-control" placeholder="Unit">
                                 </div>
                                 <div class="col">
-                                    <input type="text" name="vendorroad"class="form-control" placeholder="Road Name ">
+                                    <input type="text" name="vendorroad"class="form-control" placeholder="Bill Description ">
+                                </div>
+                                <div class="col">
+                                    <input type="text" name="vendorroad"class="form-control" placeholder="Rate">
                                 </div>
                               
+                                <div class="col">
+                                    <input type="text" name="vendorroad"class="form-control" placeholder="Total">
+                                </div>
+                              
+
                             </div>
                             
 							<div class="form-group row">
                                 <div class="col">
-                                     <input type="text" name="vendodistrict" class="form-control" placeholder="Disrict">
+                                     <input type="text" name="vendodistrict" class="form-control" placeholder="Supplier ID">
                                 </div>
                                 <div class="col">
-                                     <input type="text" name="vendorarea" class="form-control" placeholder="Area">
+                                     <input type="text" name="vendorarea" class="form-control" placeholder="Customer Id">
                                 </div>
                                 <div class="col">
-                                    <input type="text" name="vendorcity"class="form-control" placeholder="City">
+                                    <input type="text" name="vendorcity"class="form-control" placeholder="GRN no">
                                 </div>
                                 <div class="col">
-                                    <input type="text" name="vendorctry"class="form-control" placeholder="Country">
+                                    <input type="text" name="vendorctry"class="form-control" placeholder="Box No">
+                                </div>
+                                <div class="col">
+                                    <input type="text" name="vendorctry"class="form-control" placeholder="Remarks">
                                 </div>
                               
                             </div>
-                            
-							<div class="form-group row">
-                                <div class="col">
-                                     <input type="text" name="vendorzip" class="form-control" placeholder="Zip code">
-                                </div>
-                                <div class="col">
-                                     <input type="text" name="vendorprimarybuis"class="form-control" placeholder="Primary Business">
-                                </div>
-                                <div class="col">
-                                    <input type="text" name="vendorcontactpersonsir"class="form-control" placeholder="Contact Person / Surname">
-                                </div>
-                                <div class="col">
-                                    <input type="text" name="vendorothername"class="form-control" placeholder="First Name">
-                                </div>
-                              
-                            </div>
-                            <div class="form-group row">
-                                <div class="col">
-                                     <input type="text" name="vendorsocialaccount"class="form-control" placeholder="vendorsocialaccount">
-                                </div>
-                                <div class="col">
-                                     <input type="text" name="vendormobile1"class="form-control" placeholder="vendormobile1">
-                                </div>
-                                <div class="col">
-                                    <input type="text" name="vendormobile2"class="form-control" placeholder="vendormobile2">
-                                </div>
-                                <div class="col">
-                                    <input type="text" name="vendoremail1"class="form-control" placeholder="vendoremail1">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <div class="col">
-                                     <input type="text" name="vendoremail2"class="form-control" placeholder=vendoremail2">
-                                </div>
-                                <div class="col">
-                                     <input type="text" name="vendorbank"class="form-control" placeholder="vendorbank">
-                                </div>
-                                <div class="col">
-                                    <input type="text" name="vendoraccountno1"class="form-control" placeholder="vendoraccountno1">
-                                </div>
-                                <div class="col">
-                                    <input type="text" name="vendoraccountholdername1"class="form-control" placeholder="vendoraccountholdername1">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <div class="col">
-                                     <input type="text" name="vendorbank2" class="form-control" placeholder="vendorbank2">
-                                </div>
-                                <div class="col">
-                                     <input type="text" name="vendoraccountno2" class="form-control" placeholder="vendoraccountno2">
-                                </div>
-                                <div class="col">
-                                     <input type="text" name="vendoraccountholdername2" class="form-control" placeholder="vendoraccountholdername2">
-                                </div>
-                                
-                                <div class="col">
-                                     <input type="text" name="vendorwebsite" class="form-control" placeholder="vendorwebsite">
-                                </div>
+                      
+                      
+                      
                              </div> 
                                 <div class="col">
                                 <button type="submit" class="btn btn-warning">
@@ -117,6 +73,8 @@
 		<tr>
         <th scope="col">CUSTOMER </th>
         <th scope="col">SUPPLIER </th>
+        <th scope="col">Box No </th>
+        <th scope="col">GRN No</th>
         <th scope="col">ITEM No </th>
         <th scope="col">ITEM Qty </th>
 		<th scope="col">ITEM Name </th>
@@ -130,6 +88,8 @@
 		<tr>
         <td>{{$item->custid}}</td>
         <td>{{$item->suppid}}</td>
+        <td>{{$item->itmeno}}</td>
+        <td>{{$item->itmeno}}</td>
         <td>{{$item->itmeno}}</td>
         <td>{{$item->qty}}</td>
 			<td>{{$item->item_name}}</td>
