b5067cab0ab599a8dc9868b60e04d51d77f14ad9
ibrasim <asim.19824@gmail.com>
2025-06-25 20:53:31 +0500

    sdsdf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,7 +61,7 @@
                         <!-- <a class="nav-link"href="{{route('addvendor')}}">Supplier</a> -->
                         <!-- <a class="nav-link"href="{{route('billentry')}}">Data Entry</a> -->
                         <!-- <a class="nav-link"href="{{route('customersearch')}}">CustomerSearch</a> -->
-                        <a class="nav-link"href="{{route('tariffsearch')}}">TariffSearch</a>
+                        <!-- <a class="nav-link"href="{{route('tariffsearch')}}">TariffSearch</a> -->
                         <!-- <a class="nav-link"href="{{route('dashboard')}}">Dashboard</a> -->
                         <!-- <a class="nav-link"href="{{route('maintenancelog')}}">Maintenance Log</a> -->
                     </ul>
