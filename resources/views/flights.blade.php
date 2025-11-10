0ba787121adc24c64129ec78f314839624dae532
ibrasim <asim.19824@gmail.com>
2025-11-10 06:53:21 +0500

    sdf

--- resources/views/flights.blade.php
+++ resources/views/flights.blade.php
@@ -39,7 +39,7 @@
                         <td class="border border-gray-300 px-4 py-2">22 NOV 2055-2250 23 NOV 0805-1150 26 NOV 1340-1700 26 NOV 1850-1945</td>
                            <td class="border border-gray-300 px-4 py-2">YES</td>
                            <td class="border border-gray-300 px-4 py-2">IYA,SHA</td>   
-                            <td class="border border-gray-300 px-4 py-2">TO ISSUE</td>                    
+                            <td class="border border-gray-300 px-4 py-2">ISSUED</td>                    
                     </tr>
                     <tr><td></td></tr>
                    
@@ -52,7 +52,7 @@
                         <td class="border border-gray-300 px-4 py-2">25 NOV 1205-1720 25 NOV 1010-2120 29 NOV 1940-2305 30 NOV 0200-0715</td>
                            <td class="border border-gray-300 px-4 py-2">NA</td>
                            <td class="border border-gray-300 px-4 py-2">HAM,FIZ</td>   
-                            <td class="border border-gray-300 px-4 py-2">TO ISSUE</td>                    
+                            <td class="border border-gray-300 px-4 py-2">ISSUED</td>                    
                     </tr>
                    
       <tr><td></td></tr>
@@ -66,23 +66,37 @@
                         <td class="border border-gray-300 px-4 py-2">25 NOV 1205-1720 25 NOV 1010-2120 29 NOV 1940-2305 30 NOV 0200-0715</td>
                            <td class="border border-gray-300 px-4 py-2">NA</td>
                            <td class="border border-gray-300 px-4 py-2">AGE</td>   
-                            <td class="border border-gray-300 px-4 py-2">TO CONFRIM</td>                    
+                            <td class="border border-gray-300 px-4 py-2">ISSUED</td>                    
                     </tr>
  <tr><td></td></tr>
  <tr class="hover:bg-gray-50">
  
-                        <td class="border border-gray-300 px-4 py-2">26-28</td>
-                        <td class="border border-gray-300 px-4 py-2">25-29</td>
+                        <td class="border border-gray-300 px-4 py-2">26 NOV 08 DEC</td>
+                        <td class="border border-gray-300 px-4 py-2">25 NOV 10 DEC</td>
                         <td class="border border-gray-300 px-4 py-2">AI,FZ</td>
                           <td class="border border-gray-300 px-4 py-2"> MLEDEL-DELKTM </td>
                         <td class="border border-gray-300 px-4 py-2">25 NOV 1205-1720 25 NOV 1010-2120 </td>
                            <td class="border border-gray-300 px-4 py-2">NA</td>
                            <td class="border border-gray-300 px-4 py-2">NIH</td>   
-                            <td class="border border-gray-300 px-4 py-2">SOM </td>                    
+                            <td class="border border-gray-300 px-4 py-2">PENDING </td>                    
                     </tr>
 
      <tr><td></td></tr>    
+      <tr class="hover:bg-gray-50">
+ 
+                        <td class="border border-gray-300 px-4 py-2">08 -10 DEC</td>
+                        <td class="border border-gray-300 px-4 py-2">07- 10 DEC</td>
+                        <td class="border border-gray-300 px-4 py-2">AI,UL</td>
+                          <td class="border border-gray-300 px-4 py-2"> MLEDELKTM-KTMDELMLE </td>
+                        <td class="border border-gray-300 px-4 py-2">7 DEC 1205-1720 7 DEC 1910-2120 10 DEC 1340-1700 10 DEC1850-1945 </td>
+                           <td class="border border-gray-300 px-4 py-2">NA</td>
+                           <td class="border border-gray-300 px-4 py-2">SHA</td>   
+                            <td class="border border-gray-300 px-4 py-2">PENDING </td>                    
+                    </tr>
+
+
      
+     <tr><td></td></tr>   
      <tr class="hover:bg-gray-50">
  
                         <td class="border border-gray-300 px-4 py-2">01-03</td>
@@ -92,7 +106,7 @@
                         <td class="border border-gray-300 px-4 py-2">29 NOV 2055-2250 30 NOV 0805-1150 04 DEC 1250-1610 04 DEC 1850-1945 </td>
                            <td class="border border-gray-300 px-4 py-2">NA</td>
                            <td class="border border-gray-300 px-4 py-2">SHA</td>   
-                            <td class="border border-gray-300 px-4 py-2">TO ISSUE </td>                    
+                            <td class="border border-gray-300 px-4 py-2">ISSUED </td>                    
                     </tr>
      <tr><td></td></tr>    
 
@@ -105,7 +119,7 @@
                         <td class="border border-gray-300 px-4 py-2">29 NOV 2055-2250 30 NOV 0805-1150 04 DEC 1250-1610 04 DEC 1850-1945 </td>
                            <td class="border border-gray-300 px-4 py-2">NA</td>
                            <td class="border border-gray-300 px-4 py-2">NIY,HAS,NAU,ZAK</td>   
-                            <td class="border border-gray-300 px-4 py-2">TO ISSUE </td>                    
+                            <td class="border border-gray-300 px-4 py-2">TISSUED</td>                    
                     </tr>
    <tr><td></td></tr>   
      <tr class="hover:bg-gray-50">
@@ -117,7 +131,7 @@
                         <td class="border border-gray-300 px-4 py-2">30 NOV 2055-2250 01 DEC 0805-1150 04 DEC 1250-1610 04 DEC 1850-1945 </td>
                            <td class="border border-gray-300 px-4 py-2">YES</td>
                            <td class="border border-gray-300 px-4 py-2">IFT</td>   
-                            <td class="border border-gray-300 px-4 py-2">TO ISSUE </td>                    
+                            <td class="border border-gray-300 px-4 py-2">ISSUED</td>                    
                     </tr>
 
 
