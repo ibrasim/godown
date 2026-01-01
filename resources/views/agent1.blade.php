ff26563962d72fa5ddde46dd59556c0fc9f06333
ibrasim <asim.19824@gmail.com>
2026-01-02 03:50:56 +0500

    sdf

--- resources/views/agent1.blade.php
+++ resources/views/agent1.blade.php
@@ -9,8 +9,194 @@
 
                 <div class="card-body">
                     
+
+                <!-- from app .blade -->
                test 2 jan 2026
                  
+
+                               <center>
+<h2>DASH BOARD</h2></center>
+
+  <a class="nav-link link-primary" href="{{route('uaelist')}}">UAE November 2025</a>
+                      <a class="nav-link link-danger" href="{{route('bkknov25')}}">BKK November  2025</a>
+                      <a class="nav-link link-danger" href="{{route('lkdec25')}}">LK December  2025</a>
+                      <a class="nav-link link-danger" href="{{route('flights')}}">REORDER LIST 2026</a>
+                    
+                      <h5> list of boats to addu city [Saimaa,maarandhoo,nafa,sunny,ihadhoo,south express,seamax,naza,almaa,south line, shazee 4] </h5>
+
+                      
+                      first payment  UAE  MVR 15,000.00 shipment transfered on  30 november 2025 <br>
+second payment UAE MVR 20,000.00  transfered on  23 December 2025 
+ <hr>
+<h6>items purchased (November) in bkk near hotel THB 575.00 | US$ 19.00 |  MVR 390.00 </h6>
+<h6> one item  (5 doz underwear  L ) from bkk shipment included in the package - payment in bkk  list - to check</h6>
+<hr>
+
+                      <table>
+  <tr>
+     <th>qty</th>
+    <th>item name</th>
+   
+    
+    <th>price</th>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>slipper</td>
+    
+    
+    <td>140.00</td>
+  </tr>
+  <tr>
+     <td>1.0</td>
+    <td>rope</td>
+    
+    <td>50.00</td>
+  </tr>
+
+  <tr>
+     <td>1.0</td>
+    <td>kinfe</td>
+    
+    <td>90.00</td>
+  </tr>
+
+   <tr>
+      <td>5.0</td>
+    <td>steel wool</td>
+   
+    <td>60.00</td>
+  </tr>
+
+   <tr>
+      <td>1.0</td>
+    <td>sieve small</td>
+   
+    <td>25.00</td>
+  </tr>
+     <tr>
+      <td>1</td>
+    <td>sieve medium</td>
+   
+    <td>25.00</td>
+  </tr>
+   <tr>
+      <td>1</td>
+    <td>sieve large</td>
+   
+    <td>30.00</td>
+  </tr>
+  
+   <tr>
+      <td>1</td>
+    <td>table tannis ball set </td>
+   
+    <td>25.00</td>
+  </tr>
+   <tr>
+      <td>2</td>
+    <td>spoon </td>
+   
+    <td>80.00</td>
+  </tr>
+   <tr>
+      <td>1</td>
+    <td>mobile phone stand</td>
+   
+    <td>50.00</td>
+  </tr>
+</table>
+
+
+
+
+                      <hr>
+<h6>items purchased  (December) in katmandu  NPR 19,500.00 | US$ 138.00 |  MVR 2904.00 </h6>
+
+                      <table>
+  <tr>
+     <th>qty</th>
+    <th>item name</th>
+   
+    
+    <th>price</th>
+  </tr>
+  <tr>
+    <td>12.0</td>
+    <td>sneaker</td>
+    
+    
+    <td>9000.00</td>
+  </tr>
+  <tr>
+     <td>4.0</td>
+    <td>sneaker</td>
+    
+    <td>7000.00</td>
+  </tr>
+
+  <tr>
+     <td>2.0</td>
+    <td>spoon steel medium</td>
+    
+    <td>900.00</td>
+  </tr>
+
+   <tr>
+      <td>2.0</td>
+    <td>spoon steel large</td>
+   
+    <td>1200.00</td>
+  </tr>
+
+   <tr>
+      <td>2.0</td>
+    <td>band aid x 2 jar</td>
+   
+    <td>400.00</td>
+  </tr>
+     <tr>
+      <td>24.0</td>
+    <td>lip balm</td>
+   
+    <td>1200.00</td>
+  </tr>
+</table>
+
+
+<hr>
+
+ <hr>
+<h6>items purchased  (December 2025/ Janurary 2026) in Colombo  LKR 0,000.00 | US$ 0.00 |  MVR 0.00 </h6>
+
+                      <table border="1">
+  <tr>
+     <th>qty</th>
+    <th>item name</th>
+    <th>price</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>toy</td>  
+    <td>300.00</td>
+  </tr>
+  <tr>
+    <td>8</td>
+    <td>slippers</td>  
+    <td>300.00</td>
+  </tr>
+   <tr>
+    <td>2</td>
+    <td>hot cassarole set</td>  
+    <td>6000.00</td>
+  </tr>
+  
+  
+
+
+</table>
+
+               <!-- end of app .blade -->
                 </div>
                 <div>
                     
